c3d1db6956e330619bab5b14987bfe884d6ce6fd
VadMack <makarovvadim01@icloud.com>
2022-01-14 12:16:47 +0300

    PropertySources annotation usage

--- src/main/java/com/vadmack/mongodbtest/MongoDbTestApplication.java
+++ src/main/java/com/vadmack/mongodbtest/MongoDbTestApplication.java
@@ -3,8 +3,14 @@ package com.vadmack.mongodbtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@PropertySources({
+        @PropertySource("classpath:application-env.properties"),
+        @PropertySource(value = "classpath:application-env.local.properties", ignoreResourceNotFound = true)
+})
 public class MongoDbTestApplication {
 
     public static void main(String[] args) {
