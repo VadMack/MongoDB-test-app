744a3e4bed81ca34269209e2d5954b25de14e3de
VadMack <makarovvadim01@icloud.com>
2022-01-18 22:28:00 +0300

    Logging

--- src/main/java/com/vadmack/mongodbtest/controller/ProjectController.java
+++ src/main/java/com/vadmack/mongodbtest/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.vadmack.mongodbtest.controller;
 
 import com.vadmack.mongodbtest.dto.ProjectDto;
 import com.vadmack.mongodbtest.dto.ProjectNoIdDto;
+import com.vadmack.mongodbtest.log.LogRequests;
 import com.vadmack.mongodbtest.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,7 @@ public class ProjectController {
 
     private final ProjectService service;
 
+    @LogRequests
     @GetMapping
     public ResponseEntity<List<ProjectDto>> findList(
             @RequestParam(value = "filterProjectName", required = false) String name,
@@ -26,17 +28,20 @@ public class ProjectController {
         return ResponseEntity.ok(service.findList(name, pageNumber, pageSize));
     }
 
+    @LogRequests
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProjectDto> findById(@PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+    @LogRequests
     @PostMapping
     public ResponseEntity<?> create(@Valid @RequestBody ProjectNoIdDto projectNoIdDto) {
         service.create(projectNoIdDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @LogRequests
     @PutMapping(value = "/{id}")
     public ResponseEntity<?> update(@PathVariable(value = "id") Long id,
                                     @Valid @RequestBody ProjectNoIdDto projectNoIdDto) {
@@ -44,6 +49,7 @@ public class ProjectController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @LogRequests
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
