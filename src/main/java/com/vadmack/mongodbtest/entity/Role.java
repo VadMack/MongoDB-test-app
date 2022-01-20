448bbe053a67e92ff7a1694bb11f3444bc9d48af
VadMack <makarovvadim01@icloud.com>
2022-01-20 19:13:56 +0300

    Basic authentication

---
+++ src/main/java/com/vadmack/mongodbtest/entity/Role.java
@@ -0,0 +1,17 @@
+package com.vadmack.mongodbtest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements GrantedAuthority {
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    private String authority;
+}
